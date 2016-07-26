e74947c209f346fe409c6f777de80b2420bdc8e0
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-26 16:08:58 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map(
+  function(robot){
+    var copyRobot = {};
+    copyRobot['name'] = robot.name
+    if (knownDecepticons.includes(robot.name)){copyRobot['alliance'] = 'decepticon'}
+    else{copyRobot['alliance'] = 'autobot'}
+    return copyRobot
+  }
+)
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +31,16 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+counter = 0
+
+var coloredZebraStripes = zebraStripes.map(
+  function(zebra){
+  var newZebra={}
+  newZebra.width = zebra.width
+  counter ++
+  if (counter%2==0){newZebra.color='white'}
+  else{newZebra.color='black'}
+  return newZebra
+  }
+)
